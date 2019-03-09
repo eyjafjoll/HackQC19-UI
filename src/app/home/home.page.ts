eb0941fef6fbcff616e061e9a90e8432d29789ee
Hamtara <delalleau.christelle@gmail.com>
2019-03-09 17:03:31 -0500

    aweaweawe

--- src/app/home/home.page.ts
+++ src/app/home/home.page.ts
@@ -1,12 +1,27 @@
 import { Component } from '@angular/core';
 
+// Models
+import { User } from '../../models/user';
+
+// Providers
+import { UserProvider } from '../../providers/user-provider.ts'
+
 @Component({
   selector: 'app-home',
   templateUrl: 'home.page.html',
   styleUrls: ['home.page.scss'],
 })
 export class HomePage {
+
+    constructor(private userProvider: UserProvider) {
+        this.userProvider.initDB();
+    }
+
 	segmentChanged(ev: any) {
         console.log('Segment changed', ev);
+    }
+
+    ionViewDidEnter() {
+        this.users = this.userProvider.getAllItems();
     }
 }
