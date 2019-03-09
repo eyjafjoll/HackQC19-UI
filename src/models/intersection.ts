13d68497590bb433a72273b7436125a9f1aae6b1
Hamtara <delalleau.christelle@gmail.com>
2019-03-09 11:25:32 -0500

    Add model for the app

--- src/models/intersection.ts
+++ src/models/intersection.ts
@@ -3,18 +3,26 @@ export class Intersection {
     idIntersection: number;
     latitude: number;
     longitude: number;
+    colision: number;
+    pedestrianCross: number;
+    sidewalkSize: number;
     rating: number;
 
-
     constructor(
-        in_idIngredient: number,
-        in_name: number,
-        in_image: number,
-        in_quantity: number,
+        in_idIntersection: number,
+        in_latitude: number,
+        in_longitude: number,
+        in_colision: number,
+        in_pedestrianCross: number,
+        in_sidewalkSize: number,
+        in_rating: number,
     ) {
-        this.idIntersection = in_idIngredient;
-        this.latitude = in_name;
-        this.longitude = in_image;
-        this.rating = in_quantity;
+        this.idIntersection=in_idIntersection;
+        this.latitude = in_latitude;
+        this.longitude = in_longitude;
+        this.colision = in_colision;
+        this.pedestrianCross = in_pedestrianCross;
+        this.sidewalkSize=in_sidewalkSize;
+        this.rating = in_rating;
     }
 }
