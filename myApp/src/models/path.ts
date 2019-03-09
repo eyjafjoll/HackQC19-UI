9e5588a8b9e65909261af8b5bd173e354c8c7044
Hamtara <delalleau.christelle@gmail.com>
2019-03-09 09:02:34 -0500

    add model directory

---
+++ myApp/src/models/path.ts
@@ -0,0 +1,12 @@
+export class Path {
+    idPath: number;
+    colorCode: string;
+
+    constructor(
+        in_idPath: number,
+        in_colorCode: string
+    ) {
+        this.idPath=in_idPath;
+        this.colorCode=in_colorCode;
+    }
+}
